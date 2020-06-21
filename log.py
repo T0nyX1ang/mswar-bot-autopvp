7af90c19355b4dd6cd456faedda11d89f18749bb
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-06-21 23:57:04 +0800

    Move all logs into a separate folder and use timestamp as a unique tag

--- log.py
+++ log.py
@@ -1,5 +1,12 @@
 import logging
+import os
 import sys
+import time
+
+run_time = time.time()
+time_sec = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(run_time))
+time_msec = '%03d' % ((run_time - int(run_time)) * 1000)
+file_time_tag = time_sec + '-' + time_msec
 
 logger = logging.getLogger('autopvp')
 logger.setLevel(logging.DEBUG)
@@ -10,7 +17,7 @@ stream_handler.setFormatter(
 stream_handler.setLevel(logging.INFO)
 logger.addHandler(stream_handler)
 
-file_handler = logging.FileHandler('autopvp.log')
+file_handler = logging.FileHandler(os.path.join('log', 'autopvp(%s).log' % (file_time_tag)))
 file_handler.setFormatter(
     logging.Formatter('[%(asctime)s %(name)s] %(levelname)s: %(message)s'))
 file_handler.setLevel(logging.DEBUG)
