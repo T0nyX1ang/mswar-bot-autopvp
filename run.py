8c5a6a28ac2ea61a10297dd4a2c822e3e9679c5c
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-06-29 22:29:45 +0800

    Handle network error more gracefully

--- run.py
+++ run.py
@@ -1,18 +1,30 @@
 from autopvp import AutoPVPApp
 from log import logger
+from aiohttp.client_exceptions import ClientConnectorError
 import account_config
 import asyncio
 import traceback
+import time
 
 app = AutoPVPApp(config=account_config)
 bot_restart_counter = 1
 
-try:
-    while True:
-        logger.info('Bot running count: %d' % (bot_restart_counter))
+while True:
+    logger.info('Bot running count: %d' % (bot_restart_counter))
+
+    try:    
         asyncio.run(app.run())
-        bot_restart_counter += 1
-except KeyboardInterrupt as e:
-    logger.info('Bot stopped now ...')
-except Exception as e:
-    logger.critical(traceback.format_exc())
+    except ClientConnectorError:
+        logger.warning('The connection is down, please check your connection ...')
+    except KeyboardInterrupt:
+        break    
+    except Exception as e:
+        break
+        logger.critical(traceback.format_exc())
+
+    bot_restart_counter += 1
+
+    logger.info('Restarting the bot in 3 seconds ...')
+    time.sleep(3)
+
+logger.info('Bot stopped now ...')
