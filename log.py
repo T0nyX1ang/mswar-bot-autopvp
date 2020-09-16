81db06e9dd77f589956933ebd280a3adcf13083b
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-09-16 21:23:35 +0800

    Update log.py

--- log.py
+++ log.py
@@ -14,11 +14,11 @@ logger.setLevel(logging.DEBUG)
 stream_handler = logging.StreamHandler(sys.stdout)
 stream_handler.setFormatter(
     logging.Formatter('[%(asctime)s %(name)s] %(levelname)s: %(message)s'))
-stream_handler.setLevel(logging.INFO)
+stream_handler.setLevel(logging.DEBUG)
 logger.addHandler(stream_handler)
 
 file_handler = logging.FileHandler(os.path.join('log', 'autopvp(%s).log' % (file_time_tag)))
 file_handler.setFormatter(
     logging.Formatter('[%(asctime)s %(name)s] %(levelname)s: %(message)s'))
-file_handler.setLevel(logging.DEBUG)
+file_handler.setLevel(logging.ERROR)
 logger.addHandler(file_handler)
