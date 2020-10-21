7bbbaf86e8c96345f70d2a147d6388a6fb3716be
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2020-10-21 19:45:27 +0800

    adjust intervals in different conditions

--- run.py
+++ run.py
@@ -18,21 +18,23 @@ logger.info('Scheduler has been activated ...')
 
 while True:
     logger.info('Bot running count: %d' % (bot_restart_counter))
+    restart_interval = 3
 
     try:    
         asyncio.run(app.run())
     except ClientConnectorError:
         logger.warning('The connection is down, please check your connection ...')
+        restart_interval = 30
     except KeyboardInterrupt:
-        break    
+        break
     except Exception:
         logger.critical(traceback.format_exc())
-        break
+        restart_interval = 300
 
     bot_restart_counter += 1
 
-    logger.info('Restarting the bot in 3 seconds ...')
-    time.sleep(3)
+    logger.info('Restarting the bot in %d seconds ...' % restart_interval)
+    time.sleep(restart_interval)
 
 logger.info('Bot stopped now ...')
 scheduler.shutdown()
